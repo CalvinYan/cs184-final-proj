627164b5a7153240cebfa01a14e8e0008447806c
Calvin Yan <calvinyan@berkeley.edu>
2023-05-02 14:27:50 -0700

    ArUco detection complete

---
+++ src/calibrate_camera.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# The dimension of the chessboard
+
+dim = (9, 7)
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((dim[0] * dim[1] ,3), np.float32)
+objp[:,:2] = np.mgrid[0:dim[0],0:dim[1]].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('../build/capture_*.jpg')
+for fname in images:
+    print(fname)
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, dim, None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        print("found corners in " + fname)
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, dim, corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+print('ret = ', ret)
+print('mtx = ', mtx)
+print('dist = ', dist)
+print('rvecs = ', rvecs)
+print('tvecs = ', tvecs)
+
+img = cv.imread('calib_1.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+print( "total error: {}".format(mean_error/len(objpoints)) )
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,30 @@
 
 using namespace std;
 
+/*
+  Intrinsic camera parameters
+*/
+
+// float mtx_data[3][3] = {{9.77893519e+02, 0.00000000e+00, 6.58731620e+02},
+                    // {0.00000000e+00, 1.04158258e+03, 3.13113827e+02},
+                    // {0.00000000e+00, 0.00000000e+00, 1.00000000e+00}};
+
+// float mtx_data[3][3] = {{625.51823121,   0,         629.92259976,},
+                    // {0,         456.00878219, 282.58074249,},
+                    // {0.00000000e+00, 0.00000000e+00, 1.00000000e+00}};
+
+float mtx_data[3][3] = {{625.51823121,   0,         629.92259976,},
+                    {0,         456.00878219, 282.58074249,},
+                    {0.00000000e+00, 0.00000000e+00, 1.00000000e+00}};
+
+cv::Mat cameraMatrix(3, 3, CV_32F, mtx_data);
+
+// float dist_data[1][5] =  {{-1.61180357e-01, 4.15458974e+00, -2.53542144e-02, 3.72820230e-03, -1.98594926e+01}};
+// float dist_data[1][5] =  {{-0.0064325,   0.79430384,  0.00730585, -0.0083165,  -1.53361965}};
+float dist_data[1][5] =  {{0.17423076, -0.43449858, -0.04052579,  0.03844378, -0.2205249}};
+
+cv::Mat distCoeffs(1, 5, CV_32F, dist_data);
+
 int main(int argc, char** argv) {
 
     // if (argc != 2) {
@@ -44,6 +68,9 @@ int main(int argc, char** argv) {
     }
 
     cv::Mat frameIn;
+
+    bool paused = false;
+    int ctr = 0;
     for (;;) {
 
         cap.read(frameIn);
@@ -62,16 +89,53 @@ int main(int argc, char** argv) {
 
         if (ids.size() > 0) {
             cv::aruco::drawDetectedMarkers(frameOut, corners, ids);
+
+            std::vector<cv::Vec3d> rvecs, tvecs;
+
+            
+
+            // Set coordinate system
+            float objPointData[4][3] = {{ -0.5,  0.5, 0},
+                                        {0.5,  0.5, 0},
+                                        {0.5, -0.5, 0},
+                                       {-0.5, -0.5, 0}};
+
+            cv::Mat objPoints(4, 1, CV_32FC3, objPointData);
+
+            cout << objPoints << endl;
+
+
+            // Calculate pose for each marker
+            for (int i = 0; i < ids.size(); i++) {
+                rvecs.push_back(cv::Vec3d());
+                tvecs.push_back(cv::Vec3d());
+                solvePnP(objPoints, corners.at(i), cameraMatrix, distCoeffs, rvecs.at(i), tvecs.at(i), cv::SOLVEPNP_IPPE_SQUARE);
+                cout << rvecs.at(i) << endl;
+                cout << tvecs.at(i) << endl;
+
+                for (int i = 0; i < rvecs.size(); ++i) {
+                    auto rvec = rvecs[i];
+                    auto tvec = tvecs[i];
+                    cv::drawFrameAxes(frameOut, cameraMatrix, distCoeffs, rvec, tvec, 0.1);
+                }
+            }
+
         }
 
         // show live and wait for a key with timeout long enough to show images
-        cv::imshow("Live", frameOut);
+        if (!paused) {
+            cv::imshow("Live", frameOut);
+        }
 
-        int q = 'q';
+        char key = (char)(cv::waitKey(1) & 0xFF);
 
-        if ((cv::waitKey(1) & 0xFF) == q) {
+        if (key == 'q') {
             cv::destroyAllWindows();
             return 0;
+        } else if (key == 'p') {
+            paused = !paused;
+        } else if (key == 'c') {
+            cv::imwrite("capture_" + std::to_string(ctr++) + ".jpg", frameOut);
         }
     }
 
